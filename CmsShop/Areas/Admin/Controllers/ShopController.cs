fabe7aaefaf49375350bf3b082403383aaba4f14
Pawel Kepczynski <pawel_kepczynski@wp.pl>
2019-05-07 12:35:54 +0200

    Widok listy kategorii

--- CmsShop/Areas/Admin/Controllers/ShopController.cs
+++ CmsShop/Areas/Admin/Controllers/ShopController.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Shop;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace CmsShop.Areas.Admin.Controllers
@@ -9,7 +9,23 @@ namespace CmsShop.Areas.Admin.Controllers
     public class ShopController : Controller
     {
         // GET: Admin/Shop
-        public ActionResult Index()
+        public ActionResult Categories()
+        {
+            //Deklaracja listy kategori do wyswietlenia
+            List<CategoryVM> categoryVMList;
+            using (Db db = new Db())
+            {
+                categoryVMList = db.Categories
+                                .ToArray()
+                                .OrderBy(x => x.Sorting)
+                                .Select(x => new CategoryVM(x))
+                                .ToList();
+            }
+
+            return View(categoryVMList);
+        }
+        [HttpPost]
+        public ActionResult Action()
         {
             return View();
         }
